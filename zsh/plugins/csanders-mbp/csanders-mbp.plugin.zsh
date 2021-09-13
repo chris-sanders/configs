613d528e06e63206baf7f8d45a62d7beeda7b682
Chris Sanders <sanders.chris@gmail.com>
2021-09-13 11:26:33 -0500

    Add mbp zsh plugin

---
+++ zsh/plugins/csanders-mbp/csanders-mbp.plugin.zsh
@@ -0,0 +1,8 @@
+# Install brew
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Add go to path
+export PATH=$PATH:/usr/local/go/bin
+
+# Add zk (this is where it installs it)
+export PATH=$PATH:/Users/chris/go/bin
