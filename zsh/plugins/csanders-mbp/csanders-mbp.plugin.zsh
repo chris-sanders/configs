1ecee0a0ac6ede0c4c64181c991ab5c6e2250a18
Chris Sanders <sanders.chris@gmail.com>
2021-12-29 13:45:17 -0600

    update krew for mbp

--- zsh/plugins/csanders-mbp/csanders-mbp.plugin.zsh
+++ zsh/plugins/csanders-mbp/csanders-mbp.plugin.zsh
@@ -6,3 +6,6 @@ export PATH=$PATH:/usr/local/go/bin
 
 # Add zk (this is where it installs it)
 export PATH=$PATH:/Users/chris/go/bin
+
+# Add krew to the path
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
