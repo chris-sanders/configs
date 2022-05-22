21cb235c9834deac3b0835e1732a2844a5caa256
Chris Sanders <sanders.chris@gmail.com>
2022-05-22 15:13:11 -0500

    Disable zsh tmux auto window rename

--- zsh/plugins/csanders-common/csanders-common.plugin.zsh
+++ zsh/plugins/csanders-common/csanders-common.plugin.zsh
@@ -10,6 +10,9 @@ export PROMPT='%F{green}%n:%F{blue}%~%f %% '
 export PROMPT="╭$PROMPT
 ╰➤ "
 
+# Disable auto title renaming in tmux
+DISABLE_AUTO_TITLE=true
+
 # Auto tmux
 if [ -z "$TMUX" ]
 then
