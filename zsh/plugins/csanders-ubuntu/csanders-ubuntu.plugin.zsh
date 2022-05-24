8928f44f8ec92f3556b0dd32ef130f4596095152
Chris Sanders <sanders.chris@gmail.com>
2022-05-23 23:06:09 -0500

    Setup taskwarrior again

--- zsh/plugins/csanders-ubuntu/csanders-ubuntu.plugin.zsh
+++ zsh/plugins/csanders-ubuntu/csanders-ubuntu.plugin.zsh
@@ -1,3 +1,6 @@
 # Add Go
 export PATH="$PATH:/usr/local/go/bin"
 export PATH="$PATH:/home/chris/go/bin"
+
+# Change ow directory background color to be readable
+export LS_COLORS="$LS_COLORS:ow=1;31;42:"
