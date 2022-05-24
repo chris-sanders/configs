f792f9de6d01bb4f15418afb7dd4b96538a5baeb
Chris Sanders <sanders.chris@gmail.com>
2022-05-23 23:34:06 -0500

    Update task aliases

--- zsh/plugins/csanders-taskwarrior/csanders-taskwarrior.plugin.zsh
+++ zsh/plugins/csanders-taskwarrior/csanders-taskwarrior.plugin.zsh
@@ -1,8 +1,9 @@
 ## Taskwarrior
-alias tr='task ready +@work'
-alias tl='task +@lab'
-alias tw='task +@work'
-alias in='task add +in'
-alias taskl='task list limit:10 -meeting'
-alias tl=taskl
+alias tr='task ready'
+alias tl='task list limit:10'
+alias taw='task add +work'
+alias tap='task add +personal'
 alias td='task status:completed sort:end- limit:10 all'
+alias tdw='task end.after:today-1wk completed'
+alias twork='task context work'
+alias tpers='task context personal'
